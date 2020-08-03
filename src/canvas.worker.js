bafec790da4894b44af5dab18327b65756729a03
Aleksandr Kuzmin <aleksandr.kuzmin@akuzmin-laptop.spbfiller.local>
2020-08-03 19:22:19 +0300

    Redraw

--- src/canvas.worker.js
+++ src/canvas.worker.js
@@ -1,23 +1,43 @@
-onmessage = (e) => {
-  const fontFace = new FontFace('PermanentMarker', 'url(\'../fonts/PermanentMarker-Regular.ttf\')')
-  fontFace.load().then(() => {
-    this.fonts.add(fontFace)
-    const ctx = e.data.getContext('2d')
-    ctx.lineWidth = 10
-    ctx.strokeRect(75, 140, 150, 110)
-    ctx.fillRect(130, 190, 40, 60)
-    ctx.beginPath()
-    ctx.moveTo(50, 140)
-    ctx.lineTo(150, 60)
-    ctx.lineTo(250, 140)
-    ctx.closePath()
-    ctx.stroke()
+function drawHouse(ctx) {
+  ctx.fillStyle = "black";
+  ctx.lineWidth = 10
+  ctx.strokeRect(75, 140, 150, 110)
+  ctx.fillRect(130, 190, 40, 60)
+  ctx.beginPath()
+  ctx.moveTo(50, 140)
+  ctx.lineTo(150, 60)
+  ctx.lineTo(250, 140)
+  ctx.closePath()
+  ctx.stroke()
+}
 
-    ctx.font = '50px PermanentMarker'
-    ctx.fillStyle = "orangered";
-    ctx.textBaseline = "top";
-    ctx.fillText('Some text', 300, 100)
-  }).catch((err) => {
-    console.log(err)
-  })
+function drawText(ctx) {
+  ctx.font = '50px PermanentMarker'
+  ctx.fillStyle = "orangered";
+  ctx.textBaseline = "top";
+  ctx.fillText('Some text', 300, 100)
+}
+
+function draw(ctx) {
+  drawHouse(ctx)
+  drawText(ctx)
+}
+
+onmessage = (e) => {
+  switch (e.data.type) {
+    case 'init':
+      self.offscreen = e.data.offscreen
+      break
+    case 'draw':
+      if (self.fonts.size > 0) {
+        draw(self.offscreen.getContext('2d'))
+      } else {
+        const fontFace = new FontFace('PermanentMarker', 'url(\'../fonts/PermanentMarker-Regular.ttf\')')
+        fontFace.load().then((font) => {
+          self.fonts.add(font)
+          draw(self.offscreen.getContext('2d'))
+        })
+      }
+      break
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@
 
 window.onload = () => {
   const canvas = document.getElementById('canvas')
+  const button  = document.getElementById('button')
   const worker = new Worker('./src/canvas.worker.js')
-  const offscreen = canvas.transferControlToOffscreen()
-  worker.postMessage(offscreen, [offscreen])
+  var offscreen = canvas.transferControlToOffscreen()
+  worker.postMessage({ type: 'init', offscreen }, [offscreen])
+  button.onclick = () => {
+    worker.postMessage({ type: 'draw' })
+  }
 }
