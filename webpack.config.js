a9de38ce3535448aa9f1ed063784ad2071a9c8e6
Aleksandr Kuzmin <akuzmin@spbfiller.com>
2020-08-05 17:21:14 +0300

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,16 @@ const common = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.worker\.js$/,
         exclude: /node_modules/,
         loader: 'worker-loader',
